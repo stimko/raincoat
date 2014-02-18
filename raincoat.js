7354dc4b2601e49aae2651bdc1fc8b16ae899182
stimko <s.w.timko@gmail.com>
2014-02-17 22:04:37 -0500

    wip

---
+++ raincoat.js
@@ -0,0 +1,24 @@
+
+var Raincoat = {
+
+  baseURL: 'api.openweathermap.org/data/2.5/',
+
+  getWeatherFor: function(arg){
+    /* parse arguments
+      Can be coordinates, city, id
+    */
+  },
+  getWeeklyForecastFor: function(arg){
+  	/* parse arguments
+      Can be coordinates, city, id
+    */
+  },
+  searchFor: function(arg){
+     /* parse arguments
+      Can be coordinates, city, accuracy
+    */
+  },
+  inUnits: function(arg){
+    //Metric or Imperial
+  }
+}
